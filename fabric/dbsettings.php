de26e630d20e16a50ca6e053669c15101e6b38fc
Anders Fajerson <anders@perifer.se>
2012-11-06 08:49:51 +0100

    Make parsing db settings for wordpress and non-drush actually work.

--- fabric/dbsettings.php
+++ fabric/dbsettings.php
@@ -1,25 +1,34 @@
-<?php 
+<?php
 if (is_file('wp-config.php')) {
   // Wordpress
   $settings_file = 'wp-config.php';
+  // Create a temporary wp-settings.php and trick WP to load that since
+  // loading the original file here breaks stuff.
+  $temp_dir = sys_get_temp_dir() . '/';
+  file_put_contents($temp_dir . 'wp-settings.php', '');
+  define('ABSPATH', $temp_dir);
   include($settings_file);
+  $json = json_encode(array('database' => DB_NAME, 'username' => DB_USER, 'password' => DB_PASSWORD, 'host' => DB_HOST));
   exit(json_encode(array('database' => DB_NAME, 'username' => DB_USER, 'password' => DB_PASSWORD, 'host' => DB_HOST)));
 }
 else if (is_file('sites/default/settings.php')) {
   chdir('sites/default');
+  include('settings.php');
   if (isset($db_url)) {
     // Drupal 6
-    include('settings.php');
     $parsed_db_url = parse_url($db_url);
-    $parsed_db_url['database'] = substr($parsed_db_url['path'], 1);
-    $parsed_db_url['password'] = $parsed_db_url['pass'];
-    $parsed_db_url['username'] = $parsed_db_url['user'];
-    $parsed_db_url['prefix'] = $db_prefix;
-    exit(json_encode($parsed_db_url));
+    $settings['database'] = substr($parsed_db_url['path'], 1);
+    $settings['password'] = $parsed_db_url['pass'];
+    $settings['username'] = $parsed_db_url['user'];
+    $settings['host'] = $parsed_db_url['host'];
+    if (isset($parsed_db_url['port'])) {
+      $settings['port'] = $parsed_db_url['port'];
+    }
+    $settings['db_prefix'] = $db_prefix;
+    exit(json_encode($settings));
   }
   else {
     // Drupal 7
-    include('settings.php');
     exit(json_encode($databases['default']['default']));
   }
 }
